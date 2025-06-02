552514bb9e3bfecf6c38f16cdae3b86a12cc215f
theyasinsahin <yasin248sahin@gmail.com>
2025-06-02 13:03:48 +0300

    User model's variables created

--- src/model/User.java
+++ src/model/User.java
@@ -1,4 +1,15 @@
 package model;
 
+import java.util.List;
+
 public class User {
+    private int userId;
+    private String name;
+    private String email;
+    private String password;
+    private String bio;
+    private List<String> socialMedia;
+    private List<Book> books;
+    private List<User> followers;
+    private List<User> following;
 }
